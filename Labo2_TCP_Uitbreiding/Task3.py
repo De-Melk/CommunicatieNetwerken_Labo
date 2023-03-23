1967a11ef6ea2d5df7ca74798b2b9a0eb5812370
De-Melk <belmansyorrit@gmail.com>
2023-03-23 12:53:32 +0100

    .

--- Labo2_TCP_Uitbreiding/Task3.py
+++ Labo2_TCP_Uitbreiding/Task3.py
@@ -9,18 +9,21 @@ def client_server(n):
     return max((n*F)/us, F/dmin)
 
 def peer_to_peer(n, u):
-    return max(F/us, F/dmin, (n*F)/(us + sum(u)))
+    return max(F/us, F/dmin, (n*F)/(us + (u*n)))
 
 if __name__ == '__main__':
     N = [10, 100, 1000]         #Number of peers
     u = [300, 700, 2048]        #upload rate op peers (kbps)
     cs = [client_server(n) for n in N]
-    p2p = [peer_to_peer(N[i], [u[i]] * N[i]) for i in range(len(N))]
+    p2p_300 = [peer_to_peer(N[i], u[0]) for i in range(len(N))]
+    p2p_700 = [peer_to_peer(N[i], u[1]) for i in range(len(N))]
+    p2p_2048 = [peer_to_peer(N[i], u[2]) for i in range(len(N))]
 
-    plt.plot(N, cs, 'bo')
-    plt.plot(N, cs, 'b', label='Client-Server')
-    plt.plot(N, p2p, 'ro')
-    plt.plot(N, p2p, 'r', label='P2P')
+    plt.plot(N, cs, color='b', marker='o', label='Client-Server')
+    plt.plot(N, p2p_300, color='r', marker='o', label='p2p_300Kbps')
+    plt.plot(N, p2p_700, 'orange', label='p2p_700Kbps', marker='o', color='orange')
+    plt.plot(N, p2p_2048, 'yellow', label='p2p_2Mbps', marker='o', color='yellow')
+    #plt.plot(N, p2p_2048, 'o')
     plt.legend(loc='upper left')
     plt.title('Minimum Distribution Time for Client-Server vs Peer-to-Peer')
     plt.xlabel('Number of Peers')
