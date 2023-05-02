98af2e409281fee9f13b4cfc33e61eefcb787d06
De-Melk <belmansyorrit@gmail.com>
2023-05-02 19:31:34 +0200

    Task1 Added Commands

--- Labo1_TCP/client_threading.py
+++ Labo1_TCP/client_threading.py
@@ -3,29 +3,29 @@ import socket
 
 HOST = "192.168.7.67"  # The server's hostname or IP address
 PORT = 65432  # The port used by the server
-DISCONNECT_MESSAGE = "!DISCONNECT"
+DISCONNECT_MESSAGE = "!DISCONNECT"  #message to send to exit and disconnect 
 connected = True
 
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((HOST, PORT))
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)       #start TCP over ipv4 and TCP
+s.connect((HOST, PORT))                #connect to Server via IP and port
 print("[Connected to server]")
 
-def recv_msg():
+def recv_msg():                #to recveive messages
     global connected
     while connected:
-        recv_msg = s.recv(1024)
+        recv_msg = s.recv(1024)         #receive, decode and print in terminal
         print("[Server] ->",recv_msg.decode('UTF-8'))
 
-def send_msg():
+def send_msg():             #to send messages
     global connected
     while connected:
-        send_msg = input(str())
-        s.send(send_msg.encode('UTF-8'))
-        if send_msg == DISCONNECT_MESSAGE:
+        send_msg = input(str())         #send input string
+        s.send(send_msg.encode('UTF-8'))        
+        if send_msg == DISCONNECT_MESSAGE:      #stop program if disconnect message
             connected = False
 
-t = threading.Thread(target=recv_msg)
+t = threading.Thread(target=recv_msg)       #initiate, start recv_msg in other thread
 t.start()
 
 send_msg()
--- Labo1_TCP/server_threading.py
+++ Labo1_TCP/server_threading.py
@@ -3,40 +3,40 @@ import socket
 
 HOST = "192.168.7.67"  # Standard loopback interface address (localhost)
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
-DISCONNECT_MESSAGE = "!DISCONNECT"
+DISCONNECT_MESSAGE = "!DISCONNECT"  #message to send to exit and disconnect 
 ADDR = (HOST, PORT)
 connected = False
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(ADDR)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)   #start TCP over ipv4 and TCP
+s.bind(ADDR) #bind program to ip and port
 
-def recv_msg(conn, addr):
+def recv_msg(conn, addr):   #to recveive messages from client
     global connected
     while connected:
-        recv_msg = conn.recv(1024)
+        recv_msg = conn.recv(1024)          #receive, decode and print in terminal
         print(f"[{addr}] ->", recv_msg.decode('UTF-8'))
-        if recv_msg.decode('UTF-8') == DISCONNECT_MESSAGE:
+        if recv_msg.decode('UTF-8') == DISCONNECT_MESSAGE:      #exit and disconnect
             conn.sendall("[Disconnected]".encode('UTF-8'))
             conn.close()
             connected = False
             print(f"[{addr}] ->", "CLOSED THE CONNECTION!")
 
-def send_msg(conn, addr):
+def send_msg(conn, addr):       #to send messages
     global connected
     while connected:
         try:
-            send_msg = input(str())
+            send_msg = input(str())         #send input string
             conn.sendall(send_msg.encode('UTF-8'))
         except:
-            print("[No Client] can't send message when no client is connected")
+            print("[No Client] can't send message when no client is connected") #no client can't send
 
 def start():
     global connected
     while not connected:
-        conn, addr = s.accept()
+        conn, addr = s.accept()     #accept incomming clients
         print(f"[Connected] client {addr} connected")
         connected = True
-        t_recv = threading.Thread(target=recv_msg, args=(conn, addr))
+        t_recv = threading.Thread(target=recv_msg, args=(conn, addr))       #start send and receive threads
         t_recv.start()
         t_send = threading.Thread(target=send_msg, args=(conn, addr))
         t_send.start()
