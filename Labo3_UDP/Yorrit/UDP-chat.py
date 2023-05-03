e724b5373b624c3059ce200c44aa5cccd3116a36
De-Melk <belmansyorrit@gmail.com>
2023-05-03 15:22:50 +0200

    Labo3 Commands

--- Labo3_UDP/Yorrit/UDP-chat.py
+++ Labo3_UDP/Yorrit/UDP-chat.py
@@ -2,7 +2,7 @@ import socket
 import threading
 import os
 
-s = socket.socket(socket.AF_INET , socket.SOCK_DGRAM )
+s = socket.socket(socket.AF_INET , socket.SOCK_DGRAM )  #start TCP over ipv4 and UDP
 s.bind((socket.gethostname(),2222))     #server ip and portnumber
 print("\t\t\t====>  UDP CHAT APP  <=====")
 print("=============================================="*2)
@@ -10,7 +10,7 @@ nm = input("ENTER YOUR NAME : ")
 print("\nType 'quit' to exit.")
 
 #ip,port = input("Enter IP address and Port number: ").split()
-ip = socket.gethostname()       #Client ipaddres
+ip = "192.168.7.48"       #Client ipaddres
 port = 3333                     #port used by other chatter
 print("client connected on: ", ip, port)
 
